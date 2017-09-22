28a81e9d9a3b7fbf10408ddee2abe2979fbc72e2
mikaelbeat <ryynanenphm@gmail.com>
2017-09-22 16:03:33 +0300

    Table view project update

--- TableViewDemo/src/tableviewdemo/TableViewDemo.java
+++ TableViewDemo/src/tableviewdemo/TableViewDemo.java
@@ -6,10 +6,16 @@
 package tableviewdemo;
 
 import javafx.application.Application;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.TableColumn;
+import javafx.scene.control.TableView;
+import javafx.scene.control.cell.PropertyValueFactory;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,22 +27,45 @@ public class TableViewDemo extends Application {
     
     @Override
     public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+        // Define table view
         
+        // 1 - Define data set
+        ObservableList<Accounts> accountlist =
+                FXCollections.observableArrayList(
+                        new Accounts("Snake","12345","Admin"),
+                        new Accounts("Monkey","11111","Tester"),
+                        new Accounts("Cat","QWERTY","QA"),
+                        new Accounts("Fish","Chips","Manager"),
+                        new Accounts("Dog","fooD","Tester")
+                );
+
+        // 2- Define table view
+        TableView<Accounts> tvAccounts = 
+                new TableView<>(accountlist);
+        TableColumn<Accounts, String> fUserName =
+                new TableColumn<>("User Name");
+        fUserName.setCellValueFactory(new PropertyValueFactory<>("UserName"));
+        tvAccounts.getColumns().add(fUserName);
+
+        TableColumn<Accounts, String> fPassword =
+                new TableColumn<>("Password");
+        fPassword.setCellValueFactory(new PropertyValueFactory<>("Password"));
+        tvAccounts.getColumns().add(fPassword);
+
+        TableColumn<Accounts, String> fType =
+                new TableColumn<>("Account Type");
+        fType.setCellValueFactory(new PropertyValueFactory<>("Type"));
+        tvAccounts.getColumns().add(fType);
+        tvAccounts.setPrefSize(300, 350);
+        
+        
+        FlowPane root = new FlowPane();
+        root.getChildren().add(tvAccounts);
+
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("Table view DEMO");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
@@ -49,3 +78,6 @@ public class TableViewDemo extends Application {
     }
     
 }
+
+
+Video 14,18
