0381ebb925470c034cd28ed781eb02860ca08d6c
mikaelbeat <ryynanenphm@gmail.com>
2017-09-22 15:20:59 +0300

    Update

--- ListViewDemo/src/listviewdemo/ListViewDemo.java
+++ ListViewDemo/src/listviewdemo/ListViewDemo.java
@@ -6,10 +6,19 @@
 package listviewdemo;
 
 import javafx.application.Application;
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.collections.FXCollections;
+import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
 import javafx.event.EventHandler;
+import javafx.geometry.Orientation;
 import javafx.scene.Scene;
 import javafx.scene.control.Button;
+import javafx.scene.control.Label;
+import javafx.scene.control.ListView;
+import javafx.scene.control.MultipleSelectionModel;
+import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.StackPane;
 import javafx.stage.Stage;
 
@@ -21,14 +30,46 @@ public class ListViewDemo extends Application {
     
     @Override
     public void start(Stage primaryStage) {
+        
+        Label laTitle = new Label("Select admin from list");
+        Label laSelectedAdmin = new Label("You select");
+        
+        // Create list
+        // 1 - Define dataset
+        ObservableList<String> accounts = 
+                FXCollections.observableArrayList(
+                "Admin","Tester","Manager","QA");
+        
+        // 2 - Define list view and move data to it
+        ListView<String> lvAccounts = new ListView<String>(accounts);
+        lvAccounts.setPrefSize(300, 150);
+        lvAccounts.setOrientation(Orientation.VERTICAL);
+        // 3 - Define multiple selection options
+        MultipleSelectionModel<String> lvModule = 
+                lvAccounts.getSelectionModel();
+        lvModule.selectedItemProperty()
+                .addListener(new ChangeListener<String>(){
+            @Override
+            public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+                System.out.println("Selected: "+ newValue);
+                laSelectedAdmin.setText("You selected: "+newValue);
+               
+            }
+                    
+                });
+        FlowPane root = new FlowPane();
+        root.setOrientation(Orientation.VERTICAL);
+        root.getChildren().add(laTitle);
+        root.getChildren().add(lvAccounts);
+        root.getChildren().add(laSelectedAdmin);
+        
 
         
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
+
         
         Scene scene = new Scene(root, 300, 250);
         
-        primaryStage.setTitle("Hello World!");
+        primaryStage.setTitle("List view DEMO");
         primaryStage.setScene(scene);
         primaryStage.show();
     }
